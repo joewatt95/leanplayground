6e8155669e912bd50c8af0ebd66f0f2f3ee3d2f3
Joe Watt <joewatt95@u.nus.edu>
2023-05-25 12:51:52 +0800

    Use where instead of curly braces

--- lakefile.lean
+++ lakefile.lean
@@ -1,9 +1,8 @@
 import Lake
 open Lake DSL
 
-package «leanplayground» {
+package «leanplayground» where
   -- add any package configuration options here
-}
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
@@ -12,6 +11,5 @@ require smt from git
   "https://github.com/ufmg-smite/lean-smt.git" @ "main"
 
 @[default_target]
-lean_lib «Leanplayground» {
+lean_lib «Leanplayground» where
   -- add any library configuration options here
-}
