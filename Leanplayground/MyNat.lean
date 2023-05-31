1f54802ad245f0e73d1550f75ba81b036c12794e
Joe Watt <joewatt95@u.nus.edu>
2023-05-31 20:06:16 +0800

    tweaks

--- Leanplayground/MyNat.lean
+++ Leanplayground/MyNat.lean
@@ -28,12 +28,12 @@ private lemma leq'_of_leq {m} : ∀ {n}, m leq n → m leq' n
               _ = _ + 1       := by rw [h]
   show ∃ d, m + d = _ + 1 from ⟨d + 1, this⟩
 
-private lemma h : ∀ {n}, n = 0 ∨ ∃ m, n = m + 1
+private lemma eq_zero_or_succ : ∀ {n}, n = 0 ∨ ∃ m, n = m + 1
 | 0 => show _ from Or.inl rfl
 | n + 1 =>
   suffices ∃ m, n + 1 = m + 1 from Or.inr this
 
-  haveI : n = 0 ∨ ∃ m, n = m + 1 := h
+  haveI : n = 0 ∨ ∃ m, n = m + 1 := eq_zero_or_succ
   match this with
   | Or.inl (h : n = 0) =>
     haveI : n + 1 = 0 + 1 := by rw [h]
@@ -45,7 +45,14 @@ private lemma h : ∀ {n}, n = 0 ∨ ∃ m, n = m + 1
 
 private lemma leq_of_leq' {m} : ∀ {n}, (m leq' n) → m leq n
 | 0, h  =>
-  sorry
+  haveI : m = 0 ∨ ∃ d, m = d + 1 := eq_zero_or_succ
+  match this with
+  | Or.inl m_eq_0 =>
+    haveI : 0 leq 0 := Leq.LeqSelf
+    show m leq 0 by simp [m_eq_0, this]
+  | Or.inr ⟨d, m_eq_d_succ⟩ =>
+    sorry
+
 | n + 1, h => sorry
 
 end MyNat
