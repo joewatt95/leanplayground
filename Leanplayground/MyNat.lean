74a06a9444c4dbe774f39b63fbf1f3b2913464fa
joewatt95 <joewatt95@u.nus.edu>
2023-06-19 14:49:14 +0100

    prove transitivity of leq, bump mathlib

--- Leanplayground/MyNat.lean
+++ Leanplayground/MyNat.lean
@@ -17,15 +17,6 @@ lemma zero_leq : ∀ {m}, 0 leq m
 
 macro m:term "leq'" n:term : term => `(∃ d : ℕ, $m + d = $n)
 
-private lemma leq'_of_leq : ∀ {n}, m leq n → m leq' n
-| _, (Leq.LeqSelf : m leq m) =>
-  show ∃ d, m + d = m from ⟨0, rfl⟩
-| .(_ + 1), (Leq.LeqSucc m_leq_n) =>
-  haveI : m leq' _ := leq'_of_leq m_leq_n
-  let ⟨d, h⟩ := this
-  haveI : m + (d + 1) = _ + 1 := by rw [←add_assoc, h]
-  show ∃ d, m + d = _ + 1 from ⟨d + 1, this⟩
-
 private lemma eq_zero_or_succ : ∀ {n}, n = 0 ∨ ∃ m, n = m + 1
 | 0 => show _ from Or.inl rfl
 | n + 1 =>
@@ -41,6 +32,15 @@ private lemma eq_zero_or_succ : ∀ {n}, n = 0 ∨ ∃ m, n = m + 1
     haveI : n + 1 = (m + 1) + 1 := by rw [h]
     show ∃ m', n + 1 = m' + 1 from ⟨m + 1, this⟩
 
+private lemma leq'_of_leq : ∀ {n}, m leq n → m leq' n
+| _, (Leq.LeqSelf : m leq m) =>
+  show ∃ d, m + d = m from ⟨0, rfl⟩
+| .(_ + 1), (Leq.LeqSucc m_leq_n) =>
+  haveI : m leq' _ := leq'_of_leq m_leq_n
+  let ⟨d, h⟩ := this
+  haveI : m + (d + 1) = _ + 1 := by rw [←add_assoc, h]
+  show ∃ d, m + d = _ + 1 from ⟨d + 1, this⟩
+
 private lemma leq_of_leq' : ∀ {n}, (∃ d, m + d = n) → m leq n
 | n, ⟨0, h⟩ =>
   haveI : m = n := by simp at h; exact h
@@ -55,5 +55,12 @@ private lemma leq_of_leq' : ∀ {n}, (∃ d, m + d = n) → m leq n
 @[simp]
 theorem leq_iff_leq' : (m leq n) ↔ m leq' n :=
   ⟨leq'_of_leq, leq_of_leq'⟩ 
+
+theorem leq_trans : (x leq y) → (y leq z) → x leq z
+| h1, h2 =>
+  have ⟨d1, h1⟩ : ∃ d1, x + d1 = y := by simp [leq_iff_leq'] at h1; exact h1
+  have ⟨d2, h2⟩ : ∃ d2, y + d2 = z := by simp [leq_iff_leq'] at h2; exact h2
+  haveI : ∃ d, x + d = z := ⟨d1 + d2, by rw [←add_assoc, h1, h2]⟩
+  show x leq z by simp [leq_iff_leq'] at *; exact this
 
 end MyNat
