1a632fdcf3ca34f94deba70c8eb28eec1378920d
joewatt95 <joewatt95@u.nus.edu>
2023-06-20 17:47:06 +0100

    boop

--- Leanplayground/Choice/Utils.lean
+++ Leanplayground/Choice/Utils.lean
@@ -2,17 +2,15 @@ import Mathlib.Data.Set.Basic
 
 namespace Utils
 
-variable {T : Type u}
-
 @[simp]
-lemma nonempty_iff_exists : Nonempty T ↔ ∃ _ : T, True :=
+lemma nonempty_iff_exists : Nonempty α ↔ ∃ _ : α, True :=
   ⟨h1, h2⟩
   where
     h1 := λ ⟨x⟩ => show ∃ _, True from ⟨x, trivial⟩
-    h2 := λ ⟨x, _⟩ => show Nonempty T from ⟨x⟩
+    h2 := λ ⟨x, _⟩ => show Nonempty α from ⟨x⟩
 
 @[simp]
-lemma nonempty_subtype_iff_exists {X : Set T} : Nonempty X ↔ ∃ a, a ∈ X :=
+lemma nonempty_subtype_iff_exists {X : Set α} : Nonempty X ↔ ∃ a, a ∈ X :=
   ⟨h1, h2⟩
   where
     h1 := λ ⟨⟨a, a_in_X⟩⟩ => ⟨a, a_in_X⟩
