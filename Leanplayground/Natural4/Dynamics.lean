923296c92f9896d4ead9b1df314b022f402a5894
joewatt95 <joewatt95@u.nus.edu>
2023-06-23 18:00:57 +0100

    wip

--- Leanplayground/Natural4/Dynamics.lean
+++ Leanplayground/Natural4/Dynamics.lean
@@ -20,6 +20,7 @@ structure Norm where
   {deadline : Time}
   {agents : Set Agent}
   {action : Action}
+  {cond : Prop}
 
 declare_syntax_cat deontic
 syntax "MUST" : deontic
@@ -29,28 +30,38 @@ syntax "SHANT" : deontic
 syntax
   "RULE" ident
   "PARTY" term
+  "IF" term
   deontic ("DO")?
   term "BY" term
   : command
 
 macro_rules
-| `(RULE $norm:ident PARTY $party:term MUST DO $action BY $deadline)
+| `(RULE $norm:ident PARTY $party:term IF $cond MUST DO $action BY $deadline)
 => `(
     noncomputable def $norm : @Norm Time Agent Action where
       agents := return $party
       action := $action
       deontic := (Deontic.O)
       deadline := $deadline
+      cond := $cond
   )
   
 variable
-  {Borrower : Agent}
+  {Borrower Lender : Agent}
   {pay : Action}
   {deadline : Time}
 
 RULE Test
 PARTY Borrower
+IF ∃ x, (x EQUALS 0) AND x EQUALS x
 MUST DO pay BY deadline 
+
+RULE Test1
+PARTY Lender
+IF True
+MUST DO pay BY deadline
+
+-- #print Test
 
 structure Event where
 
