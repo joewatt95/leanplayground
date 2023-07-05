d775883ba4749064173afc9a3e98845a6434d5af
Joe Watt <joewatt95@u.nus.edu>
2023-07-05 16:08:24 +0800

    add PrimeSieve.lean

--- Leanplayground.lean
+++ Leanplayground.lean
@@ -1,6 +1,6 @@
 import Leanplayground.Choice.StandardChoice
 import Leanplayground.MyNat
 import Leanplayground.Natural4.Test
-import Leanplayground.Primes.LazyList
+import Leanplayground.Primes.PrimeSieve
 
 def hello := "world"
---
+++ Leanplayground/Primes/PrimeSieve.lean
@@ -0,0 +1,68 @@
+import Leanplayground.Primes.LazyList
+import Leanplayground.Primes.Utils
+
+open LazyList Utils
+
+namespace PrimeSieve
+/-
+Sieve of Eratosthenes using lazy, infinite lists.
+
+For more details, see:
+https://www.cs.hmc.edu/~oneill/papers/Sieve-JFP.pdf
+-/
+
+-- Ord is the typeclass for decidable linear orderings.
+variable [Ord α]
+
+/-
+We need to use partial here to get Lean to stop complaining about the inability
+to prove termination.
+-/
+private partial def merge : LazyList α → LazyList α → LazyList α
+  | LazyList.nil, xs
+  | xs, LazyList.nil => xs
+  | xs@(x L:: xs'), ys@(y L:: ys') =>
+    open Ordering in
+    match x <=>? y with
+    | lt => x L:: xs'.get <:merge:> ys
+    | eq => x L:: xs'.get <:merge:> ys'.get
+    | gt => y L:: xs <:merge:> ys'.get
+
+private partial def union : LazyList (LazyList α) → LazyList α
+  | (x L:: xs) L:: ys => ys.get
+    |> pairwise merge
+    |> union
+    |> (xs.get <:merge:> .)
+    |> (x L:: .)
+  | _ => default
+
+private def minus : LazyList α → LazyList α → LazyList α
+  | x L:: xs', ys@(y L:: ys') =>
+    open Ordering in
+    match x <=>? y with
+    | lt => x L:: xs'.get <:minus:> ys
+    | eq => xs'.get <:minus:> ys'.get
+    | _ => default
+  | _, _ => default
+
+partial def primes : LazyList Nat := 2 L:: oddPrimes ()
+  where
+    /-
+    While this looks like it will go into infinite recursion when called, |> and
+    <| are implemented as macros so that the recursion is guarded by the
+    L:: constructor, which is lazy in its second argument. 
+    Hence, as long as we only inspect finitely many elements at a time, the
+    corecursion will be productive.
+    -/
+    oddPrimes _ := oddPrimes ()
+      |> (oddMultsFromSquare <$> .)
+      |> union
+      |> (oddsFrom5 <:minus:> .)
+      |> (3 L:: .)
+    oddMultsFromSquare n :=
+      n |> (. ^2) |> iterate (. + 2 * n)
+    oddsFrom5 := iterate (. + 2) 5
+
+-- #eval primes |> take' 10000
+
+end PrimeSieve
