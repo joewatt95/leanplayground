6552f4d9d1337113729f007094ef39414fad5245
joewatt95 <joewatt95@u.nus.edu>
2023-07-10 10:10:12 +0800

    import and compile TestSubtyping

--- Leanplayground.lean
+++ Leanplayground.lean
@@ -1,6 +1,7 @@
 import Leanplayground.Choice.StandardChoice
 import Leanplayground.MyNat
 import Leanplayground.Natural4.Test
+import Leanplayground.Natural4.TestSubtyping
 import Leanplayground.Primes.PrimeSieve
 
 def hello := "world"
