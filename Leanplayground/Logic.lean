e0555b1f2ba66be50f644d352ad702f8f563538a
Joe Watt <joewatt95@u.nus.edu>
2023-08-03 23:51:32 +0800

    tweaks

--- Leanplayground/Logic.lean
+++ Leanplayground/Logic.lean
@@ -2,10 +2,12 @@ namespace Logic
 
 variable {P : Prop} {Q : α → Prop}
 
+-- CPS unpacking of existential type.
 example : (∀ τ, Q τ → P) → (∃ τ, Q τ) → P :=
-  λ h ⟨τ, hQa⟩ => h τ hQa
+  λ κ ⟨τ, h⟩ => κ τ h
 
+-- CPS packing of existential type.
 example : ((∃ τ, Q τ) → P) → ∀ τ, Q τ → P :=
-  λ h τ hQa => h ⟨τ, hQa⟩
+  λ κ τ h => κ ⟨τ, h⟩
 
 end Logic
