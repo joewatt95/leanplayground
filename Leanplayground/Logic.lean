51265a66e2a4ad6a62d1cb8391e1d56ce650495c
joewatt95 <joewatt95@u.nus.edu>
2023-08-04 15:40:44 +0800

    boop

--- Leanplayground/Logic.lean
+++ Leanplayground/Logic.lean
@@ -4,10 +4,10 @@ variable {P : Prop} {Q : α → Prop}
 
 -- CPS unpacking of existential type.
 example : (∀ τ, Q τ → P) → (∃ τ, Q τ) → P :=
-  λ κ ⟨τ, h⟩ => κ τ h
+  λ κ ⟨τ, π⟩ => κ τ π
 
 -- CPS packing of existential type.
 example : ((∃ τ, Q τ) → P) → ∀ τ, Q τ → P :=
-  λ κ τ h => κ ⟨τ, h⟩
+  λ κ τ π => κ ⟨τ, π⟩
 
 end Logic
