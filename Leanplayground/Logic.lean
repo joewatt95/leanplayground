8c0f44bed86e0bcd30667e93c2cf4448b1d9839e
Joe Watt <joewatt95@u.nus.edu>
2024-01-29 20:00:54 +0800

    bump

--- Leanplayground/Logic.lean
+++ Leanplayground/Logic.lean
@@ -1,9 +1,10 @@
 namespace Logic
 
-example {Q : α → Prop} : (∀ a, Q a → P) ↔ (∃ a, Q a) → P :=
-  ⟨uncurry, curry⟩
-  where
-    uncurry f pair := let ⟨a, b⟩ := pair; f a b
-    curry f a b := f ⟨a, b⟩
+example {Q : α → Prop} : (∀ a, Q a → P) ↔ (∃ a, Q a) → P
+where
+  -- uncurry
+  mp f pair := let ⟨a, b⟩ := pair; f a b
+  -- curry
+  mpr f a b := f ⟨a, b⟩
 
 end Logic
