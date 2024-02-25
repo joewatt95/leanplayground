8ec92957b6831810c550ca385ed219786a7e7f08
Joe Watt <joewatt95@u.nus.edu>
2024-02-25 14:02:42 +0800

    bump libs

--- lakefile.lean
+++ lakefile.lean
@@ -8,8 +8,13 @@ package leanplayground where
     "-lctranslate2"
   ]
 
+@[default_target]
+lean_lib Leanplayground where
+  -- add any library configuration options here
+
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "v4.6.0-rc1"
+  "https://github.com/leanprover-community/mathlib4"
+    @ "a36350f2089912de4d11fbccb92d7575bf9f28e0"
 
 require verso from git
   "https://github.com/leanprover/verso" @ "main"
@@ -21,20 +26,17 @@ require verbose from git
   "https://github.com/PatrickMassot/verbose-lean4"
 
 require auto from git
-  "https://github.com/leanprover-community/lean-auto" @ "v4.6.0-rc1"
+  "https://github.com/leanprover-community/lean-auto" @ "v0.0.7"
 
 require Duper from git
   "https://github.com/leanprover-community/duper" @ "v0.0.6"
 
 require loogle from git
   "https://github.com/nomeata/loogle"
+    @ "c6903e48f1441118b6d27489d884eb0893278e2f"
 
 -- require smt from git
 --   "https://github.com/ufmg-smite/lean-smt.git" @ "main"
 
 -- require LeanCodePrompts from git
 --   "https://github.com/siddhartha-gadgil/LeanAide" @ "main"
-
-@[default_target]
-lean_lib Leanplayground where
-  -- add any library configuration options here
