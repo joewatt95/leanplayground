db831d0632528671ec2da69a8960a205ce9d6234
Joe Watt <joewatt95@u.nus.edu>
2024-03-04 18:07:52 +0800

    bump

--- lakefile.lean
+++ lakefile.lean
@@ -14,16 +14,18 @@ lean_lib Leanplayground where
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4"
-    @ "a36350f2089912de4d11fbccb92d7575bf9f28e0"
+    @ "dad5e8c449e037e72c9890baa70a85b2a307db68"
 
 require verso from git
-  "https://github.com/leanprover/verso" @ "main"
+  "https://github.com/leanprover/verso"
+    @ "77540864f089f12017a6aa2585b31130271bc111"
 
 require LeanCopilot from git
   "https://github.com/lean-dojo/LeanCopilot" @ "v1.1.1"
 
 require verbose from git
   "https://github.com/PatrickMassot/verbose-lean4"
+    @ "3c49648eb4151626143b6a45234d6f13ae7582d0"
 
 require auto from git
   "https://github.com/leanprover-community/lean-auto" @ "v0.0.7"
