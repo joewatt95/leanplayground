370ae89e0a09589423e23c252e05da1623eaea56
Joe Watt <joewatt95@u.nus.edu>
2024-05-04 22:30:51 +0800

    cleanup

--- Leanplayground/MathInLean/Sets/Csb.lean
+++ Leanplayground/MathInLean/Sets/Csb.lean
@@ -1,3 +1,5 @@
+import Auto
+import Duper
 import Mathlib.Data.Set.Lattice
 import Mathlib.Order.FixedPoints
 import Mathlib.SetTheory.Ordinal.FixedPointApproximants
@@ -63,48 +65,41 @@ theorem bij_of_2_inj
 
   let h a := if _ : a ∈ S₀ then invFun g a else f a
 
-  have := calc
-        g '' (f '' S₀ᶜ)ᶜ
-      = g '' (f '' (OrderHom.lfp F)ᶜ)ᶜ := by aesop
-    _ = OrderHom.lfp F                 := by exact F.map_lfp
-    _ = S₀                             := by aesop
+  have : g '' (f '' S₀ᶜ)ᶜ = S₀ :=
+    have : S₀ = OrderHom.lfp F := by aesop
+    by rw [this]; exact F.map_lfp
 
   have g_surjects {a} (_ : a ∈ S₀) : ∃ b ∈ (f '' S₀ᶜ)ᶜ, g b = a :=
     have : a ∈ g '' (f '' S₀ᶜ)ᶜ := by aesop
     by rw [←mem_image]; exact this
 
-  have : LeftInverse (invFun g) g := leftInverse_invFun g_inj
+  have g_inv_left_inv : LeftInverse (invFun g) g := leftInverse_invFun g_inj
+
   have := calc
         (f '' S₀ᶜ)ᶜ
-      = invFun g '' (g '' (f '' S₀ᶜ)ᶜ)  := by rw [this.image_image]
-    _ = invFun g '' S₀                  := by aesop
+    _ = invFun g '' (g '' (f '' S₀ᶜ)ᶜ) := by rw [g_inv_left_inv.image_image]
+    _ = invFun g '' S₀                 := by aesop
 
   have : Surjective h := piecewise_is_surj <| calc
         invFun g '' S₀ ∪ f '' S₀ᶜ
-      = (f '' S₀ᶜ)ᶜ ∪ f '' S₀ᶜ    := by aesop
-    _ = univ                      := by ext _; simp only [compl_union_self, mem_univ]
+    _ = (f '' S₀ᶜ)ᶜ ∪ f '' S₀ᶜ    := by aesop
+    _ = univ                      := by simp only [compl_union_self]
 
   have : Injective h :=
     have := calc
           invFun g '' S₀ ∩ f '' S₀ᶜ
       _ = (f '' S₀ᶜ)ᶜ ∩ f '' S₀ᶜ    := by aesop
-      _ = ∅                         := by ext _; simp only [compl_inter_self, mem_empty_iff_false]
+      _ = ∅                         := by simp only [compl_inter_self]
 
     have : InjOn f S₀ᶜ := λ _a _ _a' _ ↦ by aesop
 
-    have : InjOn (invFun g) S₀ :=
-      λ a (_ : a ∈ S₀) a' (_ : a' ∈ S₀) (_ : invFun g a = invFun g a') ↦
-        have ⟨b, _, _⟩ : ∃ b ∈ (f '' S₀ᶜ)ᶜ, g b = a := g_surjects ‹a ∈ S₀›
-        have ⟨b', _, _⟩ : ∃ b' ∈ (f '' S₀ᶜ)ᶜ, g b' = a' := g_surjects ‹a' ∈ S₀›
-        have := ‹LeftInverse (invFun g) g›
-        calc
-          a = g b                 := by aesop
-          _ = g (invFun g $ g b)  := by exact b |> this |>.symm |> congr_arg g
-          _ = g (invFun g a)      := by aesop
-          _ = g (invFun g a')     := by aesop
-          _ = g (invFun g $ g b') := by aesop
-          _ = g b'                := by exact b' |> this |> congr_arg g
-          _ = a'                  := by aesop
+    have : InjOn (invFun g) S₀
+      | a, (_ : a ∈ S₀), a', (_ : a' ∈ S₀),
+        (_ : invFun g a = invFun g a') =>
+        have : ∃ b ∈ (f '' S₀ᶜ)ᶜ, g b = a := g_surjects ‹a ∈ S₀›
+        have : ∃ b' ∈ _, g b' = a' := g_surjects ‹a' ∈ S₀›
+        have : ∀ x, invFun g (g x) = x := g_inv_left_inv
+        show a = a' by aesop
 
     show Injective h by apply piecewise_is_inj; repeat assumption
 
