efc808633c9df490c6e2a4c1785f722e33364b4f
Joe Watt <joewatt95@u.nus.edu>
2024-05-07 15:46:31 +0800

    cleanup a bit

--- Leanplayground/MathInLean/Sets/Basic.lean
+++ Leanplayground/MathInLean/Sets/Basic.lean
@@ -1,55 +1,51 @@
-import Duper
 import Mathlib.Data.Set.Basic
 
 namespace Sets
 
-variable (s t u : Set α)
+universe u
+variable {α : Type u} (s t u : Set α)
 
 -- open Set
 
 example : s ∩ t = t ∩ s :=
-  by ext _; exact ⟨h1, h2⟩
+  by ext _; exact ⟨mp, mpr⟩
   where
-    h1 {x} : x ∈ s ∧ x ∈ t → x ∈ t ∧ x ∈ s
+    mp {x} : x ∈ s ∧ x ∈ t → x ∈ t ∧ x ∈ s
       | ⟨x_in_s, x_in_t⟩ => ⟨x_in_t, x_in_s⟩
 
-    h2 {x} : x ∈ t ∧ x ∈ s → x ∈ s ∧ x ∈ t
+    mpr {x} : x ∈ t ∧ x ∈ s → x ∈ s ∧ x ∈ t
       | ⟨x_in_t, x_in_s⟩ => ⟨x_in_s, x_in_t⟩
 
-example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u :=
-  λ x ⟨(x_in_s : x ∈ s), (x_in_u : x ∈ u)⟩ ↦
-    have : x ∈ t := h x_in_s
-    show x ∈ t ∧ x ∈ u from by duper [*]
+example (_ : s ⊆ t) : s ∩ u ⊆ t ∩ u :=
+  λ x ⟨(_ : x ∈ s), (_ : x ∈ u)⟩ ↦
+    have : x ∈ t := ‹s ⊆ t› ‹x ∈ s›
+    show x ∈ t ∧ x ∈ u by aesop
 
 example : s ∩ (t ∪ u) ⊆ s ∩ t ∪ s ∩ u :=
-  λ x ⟨x_in_s, x_in_t_or_u⟩ ↦
-    match x_in_t_or_u with
-    | .inl x_in_t =>
-      show _ from .inl (⟨x_in_s, x_in_t⟩ : x ∈ s ∩ t)
-    | .inr x_in_u =>
-      show _ from .inr (⟨x_in_s, x_in_u⟩ : x ∈ s ∩ u)
+  λ x ⟨(_ : x ∈ s), (_ : x ∈ t ∪ u)⟩ ↦
+    match ‹x ∈ t ∪ u› with
+    | .inl (_ : x ∈ t) => .inl ⟨‹x ∈ s›, ‹x ∈ t›⟩
+    | .inr (_ : x ∈ u) => .inr ⟨‹x ∈ s›, ‹x ∈ u›⟩
 
 example : s \ (t ∪ u) ⊆ (s \ t) \ u :=
-  λ x ⟨(x_in_s : x ∈ s), (x_notin_t_or_u : x ∉ t ∪ u)⟩ ↦
-    have : x ∉ t
-      | x_in_t => x_in_t |> .inl |> x_notin_t_or_u
-    have : x ∉ u
-      | x_in_u => x_in_u |> .inr |> x_notin_t_or_u
-    show x ∈ (s \ t) \ u by simp only [Set.mem_diff]; duper [*]
+  λ x ⟨(x_in_s : x ∈ s), (_ : x ∉ t ∪ u)⟩ ↦
+    have : x ∉ t := λ _ : x ∈ t ↦ ‹x ∈ t› |> .inl |> ‹x ∉ t ∪ u›
+    have : x ∉ u := λ _ : x ∈ u ↦ ‹x ∈ u› |> .inr |> ‹x ∉ t ∪ u›
+    show (x ∈ s ∧ x ∉ t) ∧ x ∉ u from ⟨⟨‹x ∈ s›, ‹x ∉ t›⟩, ‹x ∉ u›⟩
 
 example : f '' s ⊆ v ↔ s ⊆ f ⁻¹' v
 where
-  mp (fs_subset_v : f '' s ⊆ v) x (x_in_s : x ∈ s) :=
-    suffices f x ∈ v by simp only [Set.mem_preimage]; duper [*]
-    have : f x ∈ f '' s := by simp only [Set.mem_image]; duper [*]
-    show f x ∈ v from fs_subset_v this
+  mp (_ : f '' s ⊆ v) x (_ : x ∈ s) :=
+    suffices f x ∈ v by aesop
+    have : f x ∈ f '' s := by aesop
+    show f x ∈ v from ‹f '' s ⊆ v› this
 
-  mpr (s_subset_preimg_v : s ⊆ f ⁻¹' v) y (y_in_fs : y ∈ f '' s) :=
+  mpr (_ : s ⊆ f ⁻¹' v) y (_ : y ∈ f '' s) :=
     have : ∀ x ∈ s, f x ∈ v
-      | x, x_in_s =>
-        have : x ∈ f ⁻¹' v := s_subset_preimg_v x_in_s
-        by simp only [Set.mem_image, Set.mem_preimage] at *; duper [*]
-    have : ∃ x ∈ s, f x = y := by simp only [Set.mem_image] at *; duper [*]
-    show y ∈ v by duper [*]
+    | x, (_ : x ∈ s) =>
+      have : x ∈ f ⁻¹' v := ‹s ⊆ f ⁻¹' v› ‹x ∈ s›
+      by aesop
+    have : ∃ x ∈ s, f x = y := by aesop
+    show y ∈ v by aesop
 
 end Sets
