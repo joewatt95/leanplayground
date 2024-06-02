c0f33cae0b2f4b41968796f32cf18f3839786fc8
Joe Watt <joewatt95@u.nus.edu>
2024-06-02 20:15:36 +0800

    cleanup

--- Leanplayground/MathInLean/NumberTheory/InfinitelyManyPrimes.lean
+++ Leanplayground/MathInLean/NumberTheory/InfinitelyManyPrimes.lean
@@ -7,7 +7,7 @@ theorem exists_prime_factor {n : ℕ} (_ : 2 ≤ n) : ∃ p, p.Prime ∧ p ∣ n
   suffices ¬ n.Prime → ∃ p, p.Prime ∧ p ∣ n  by tauto
   λ _ : ¬ n.Prime ↦
     have : ∃ m < n, m ∣ n ∧ m ≠ 1 := by duper [*, Nat.prime_def_lt] {portfolioInstance := 1}
-    let ⟨m, (_ : m < n), (_ : m ∣ n), (_ : m ≠ 1)⟩ := this
+    have ⟨m, (_ : m < n), (_ : m ∣ n), (_ : m ≠ 1)⟩ := this
 
     have : 2 ≤ m :=
       have : m ≠ 0 := by aesop
@@ -30,7 +30,7 @@ theorem primes_infinite : ∀ {n : ℕ}, ∃ p > n, p.Prime
       have : n ! ≤ (n + 1) ! := by apply Nat.factorial_le; omega
       by omega
 
-    let ⟨p, (_ : p.Prime), (_ : p ∣ (n + 1)! + 1)⟩ := exists_prime_factor this
+    have ⟨p, (_ : p.Prime), (_ : p ∣ (n + 1)! + 1)⟩ := exists_prime_factor this
 
     suffices ¬ p ≤ n by aesop
     λ _ : p ≤ n ↦
@@ -52,7 +52,7 @@ theorem primes_infinite' : ∀ {S : Finset ℕ}, ∃ p, p.Prime ∧ p ∉ S
       have : 2 ≤ S_primes_prod := by
         duper [*, Finset.single_le_prod', Nat.Prime.one_le, Nat.prime_two] {portfolioInstance := 1}
 
-      let ⟨p, (_ : p.Prime), (_ : p ∣ S_primes_prod + 1)⟩ :=
+      have ⟨p, (_ : p.Prime), (_ : p ∣ S_primes_prod + 1)⟩ :=
         exists_prime_factor <| Nat.le_succ_of_le this
 
       have : p ∣ S_primes_prod := by duper [*, dvd_prod_of_mem] {portfolioInstance := 1}
@@ -78,17 +78,17 @@ theorem exists_prime_factor_mod_4_eq_3 {n : ℕ}
   suffices ¬ n.Prime → ∃ p, p.Prime ∧ p ∣ n ∧ p % 4 = 3 by tauto
   λ _ : ¬ n.Prime ↦
     have : ∃ m < n, m ∣ n ∧ m ≠ 1 :=
-      have : 2 ≤ n := by omega
-      by duper [*, Nat.prime_def_lt] {portfolioInstance := 1}
+      suffices 2 ≤ n by duper [*, Nat.prime_def_lt] {portfolioInstance := 1}
+      by omega
 
-    let ⟨m, (_ : m < n), (_ : m ∣ n), (_ : m ≠ 1)⟩ := this
+    have ⟨m, (_ : m < n), (_ : m ∣ n), (_ : m ≠ 1)⟩ := this
 
     have : m % 4 = 3 ∨ n / m % 4 = 3 := by
       duper [*, Nat.mul_div_cancel', mod_4_eq_3_or] {portfolioInstance := 1}
 
     match this with
     | .inl (_ : m % 4 = 3) =>
-      let ⟨p, (_ : p.Prime), (_ : p ∣ m), (_ : p % 4 = 3)⟩ :=
+      have ⟨p, (_ : p.Prime), (_ : p ∣ m), (_ : p % 4 = 3)⟩ :=
         exists_prime_factor_mod_4_eq_3 ‹m % 4 = 3›
       have := calc
         p ∣ m := ‹_›
@@ -99,15 +99,15 @@ theorem exists_prime_factor_mod_4_eq_3 {n : ℕ}
       -- This is required to justify the well-founded recursion on n / m.
       have : n / m < n :=
         suffices m ≠ 0 by apply Nat.div_lt_self; all_goals omega
-        λ _ : m = 0 ↦ nomatch calc
-          3 = n / m % 4 := Eq.symm ‹_›
-          _ = n / 0 % 4 := by rw [‹m = 0›]
-          _ = 0         := by simp only [Nat.div_zero, Nat.zero_mod]
+        λ _ : m = 0 ↦ show ⊥ by simp_all only [
+          ne_eq, zero_dvd_iff, zero_ne_one, not_false_eq_true, Nat.zero_mod,
+          OfNat.zero_ne_ofNat, Nat.div_zero, or_self
+        ]
 
-      let ⟨p, (_ : p.Prime), (_ : p ∣ n / m), (_ : p % 4 = 3)⟩ :=
+      have ⟨p, (_ : p.Prime), (_ : p ∣ n / m), (_ : p % 4 = 3)⟩ :=
         exists_prime_factor_mod_4_eq_3 ‹n / m % 4 = 3›
       have := calc
-        p ∣ n / m := by assumption
+        p ∣ n / m := ‹_›
         _ ∣ n     := Nat.div_dvd_of_dvd ‹_›
       by tauto
 
