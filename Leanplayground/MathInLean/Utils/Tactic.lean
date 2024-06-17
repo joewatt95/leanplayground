35cc734dc52963fc5a54716434e874f30af07d38
Joe Watt <joewatt95@u.nus.edu>
2024-06-17 17:39:49 +0800

    setup auto macro

--- Leanplayground/MathInLean/Utils/Tactic.lean
+++ Leanplayground/MathInLean/Utils/Tactic.lean
@@ -8,16 +8,18 @@ import Egg
 
 import Loogle.Find
 
-set_option auto.smt true
-set_option auto.smt.trust true
-set_option auto.smt.solver.name "z3"
+macro "setup_auto" : command => `(
+  set_option auto.smt true
+  set_option auto.smt.trust true
+  set_option auto.smt.solver.name "cvc5"
 
-set_option trace.auto.smt.printCommands true
-set_option trace.auto.smt.result true
+  set_option trace.auto.smt.printCommands true
+  set_option trace.auto.smt.result true
 
-set_option auto.tptp true
-set_option auto.tptp.solver.name "zipperposition"
-set_option auto.tptp.zeport.path ""
+  set_option auto.tptp true
+  set_option auto.tptp.solver.name "zipperposition"
+  set_option auto.tptp.zeport.path "/home/joe/dev/zipperposition/portfolio"
+)
 
 macro_rules | `(tactic| trivial) => `(tactic| tauto)
 
