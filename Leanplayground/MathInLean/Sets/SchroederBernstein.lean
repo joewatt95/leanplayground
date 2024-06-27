45e2d201718299b4b96ded3af3574c10cb315500
Joe Watt <joewatt95@u.nus.edu>
2024-06-27 10:23:05 +0800

    format

--- Leanplayground/MathInLean/Sets/SchroederBernstein.lean
+++ Leanplayground/MathInLean/Sets/SchroederBernstein.lean
@@ -26,10 +26,11 @@ lemma piecewise_is_inj
   let h := h (f := f) (g := g) (X := X)
 
   have : ∀ a ∈ X, ∀ a' ∉ X, h a ≠ h a' :=
-    λ a _ a' _ (_ : h a = h a') ↦ show ⊥ from calc
-      f a = g a'             := by aesop
-        _ ∈ f '' X ∩ g '' Xᶜ := ⟨by aesop, by aesop⟩
-        _ = ∅                := ‹_›
+    λ a _ a' _ (_ : h a = h a') ↦
+      show ⊥ from calc
+        f a = g a'             := by aesop
+          _ ∈ f '' X ∩ g '' Xᶜ := ⟨by aesop, by aesop⟩
+          _ = ∅                := ‹_›
 
   λ a a' (_ : h a = h a') ↦
     show a = a' from match Classical.em _, Classical.em _ with
