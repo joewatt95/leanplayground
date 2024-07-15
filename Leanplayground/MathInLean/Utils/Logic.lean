195c49e99bbe9a3f05aca00d9cd880f59fc4e039
Joe Watt <joewatt95@u.nus.edu>
2024-07-15 23:14:30 +0800

    t

--- Leanplayground/MathInLean/Utils/Logic.lean
+++ Leanplayground/MathInLean/Utils/Logic.lean
@@ -4,7 +4,7 @@ namespace Logic
 
 variable {p q r : Prop}
 
-lemma curry (_ : p ∧ q → r) (_ : p) (_ : q) : r := by tauto
+lemma curry (_ : p ∧ q → r) (_ : p) (_ : q) : r := ‹p ∧ q → r› ⟨‹p›, ‹q›⟩
 
 lemma uncurry : (p → q → r) → (p ∧ q) → r := And.elim
 
