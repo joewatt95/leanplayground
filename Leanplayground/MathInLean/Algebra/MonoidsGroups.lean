4326d9c8dd50c06506abb0a3ad41f92e3130851e
Joe Watt <joewatt95@u.nus.edu>
2024-07-16 21:51:55 +0800

    tweak simp

--- Leanplayground/MathInLean/Algebra/MonoidsGroups.lean
+++ Leanplayground/MathInLean/Algebra/MonoidsGroups.lean
@@ -89,8 +89,7 @@ instance : MulAction G (Subgroup G) where
       λ _ ↦ by group
 
     λ x ↦ show x ∈ lhs ↔ x ∈ rhs by
-      simp_all only
-        [conjugate, Subgroup.mem_mk, Set.mem_setOf_eq, lhs, rhs]
+      simp only [conjugate, Subgroup.mem_mk, Set.mem_setOf_eq, lhs, rhs]
       duper [this] {portfolioInstance := 1}
 
 end Algebra
