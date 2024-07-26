9a5beb09ba7991a7bbaed1adb809ffaadc754bc3
Joe Watt <joewatt95@u.nus.edu>
2024-07-26 15:34:48 +0800

    wip

--- Leanplayground/MathInLean/Algebra/Ring.lean
+++ Leanplayground/MathInLean/Algebra/Ring.lean
@@ -24,22 +24,39 @@ variable {ι : Type w}
 
 -- The homomorphism from ``R ⧸ ⨅ i, I i`` to ``Π i, R ⧸ I i`` featured in the Chinese
 -- Remainder Theorem.
-def crtMap (I : ι → Ideal R) : (R ⧸ ⨅ i, I i) →+* Π i, R ⧸ I i where
-  toFun :=
-    -- let f : R → Π i, R ⧸ I i :=
-    --   λ r i ↦ (r : R ⧸ I i)
+def crtMap (I : ι → Ideal R) : (R ⧸ ⨅ i, I i) →+* ∀ i, R ⧸ I i :=
+  -- Pi.ringHom allows us to use the universal property of Pi types to construct
+  -- a ring morphism into a Pi type.
+  let f_i : ∀ i, R →+* R ⧸ I i := (Ideal.Quotient.mk <| I .)
+  let f : R →+* ∀ i, R ⧸ I i := Pi.ringHom f_i
 
-    let f : R →+* Π i, R ⧸ I i := Pi.ringHom (Ideal.Quotient.mk <| I .)
+  have : RingHom.ker f = ⨅ i, I i := Ideal.ker_Pi_Quotient_mk _
 
-    sorry
+  -- Lift the morphism to the quotient ring via the corresponding universal
+  -- property.
+  let f' : R ⧸ RingHom.ker f →+* ∀ i, R ⧸ I i := f.kerLift
 
-  map_one' := sorry
+  -- The following line uses Curry-Howard to change the type of `f'`:
+  -- 1. We view f' as a proof (ie term) of the proposition (ie type)
+  --    `R ⧸ RingHom.ker f →+* ∀ i, R ⧸ I i`
+  -- 2. We then use `rwa` to do the following:
+  --    2a. It rewrites at f' using the equation `RingHom.ker f = ⨅ i, I i`, so
+  --        that `f'` is now a proof (ie term) of the proposition (ie type)
+  --        `(R ⧸ ⨅ i, I i) →+* ∀ i, R ⧸ I i`
+  --    2b. It uses `assumption` to supply `f'` as a proof (ie term) of the goal
+  --    proposition (ie desired type of the output)
+  show (R ⧸ ⨅ i, I i) →+* ∀ i, R ⧸ I i by rwa [this] at f'
 
-  map_mul' := sorry
+-- def crtMap' (I : ι → Ideal R) : (R ⧸ ⨅ i, I i) →+* ∀ i, R ⧸ I i :=
+--   Ideal.quotientInfToPiQuotient _
 
-  map_zero' := sorry
+private lemma crtMap_mk {I : ι → Ideal R} {x : R} :
+  crtMap I (Quotient.mk _ x) = fun i : ι ↦ Ideal.Quotient.mk (I i) x :=
+  by simp [crtMap]; sorry
 
-  map_add' := sorry
+private lemma crtMap_mk' {I : ι → Ideal R} {x : R} {i : ι} :
+  crtMap I (Quotient.mk _ x) i = Ideal.Quotient.mk (I i) x :=
+  sorry
 
 end CRT
 
