ab0a4942a26d1d96adfa0842b86f51127a662a9d
Joe Watt <joewatt95@u.nus.edu>
2024-08-15 23:28:44 +0800

    wip

--- Leanplayground/Prob/CVM/Algo.lean
+++ Leanplayground/Prob/CVM/Algo.lean
@@ -28,49 +28,97 @@ noncomputable abbrev initialState : State m where
 
 noncomputable abbrev thresh := Nat.ceil <| (12 / ε^2) * Real.log2 ((8*m) / δ)
 
+instance : CommApplicative PMF where
+  commutative_prod {α β} (x : PMF α) (y : PMF β) :=
+    sorry
+
 open Classical in
 noncomputable def estimateSize
   (stream : List <| Fin m)
-  : Prob.ProbM (State m) <| Fin m:= do
-  match _h_stream_eq : stream with
-  | [] =>
-    let state ← get
-    pure <| Nat.floor <| state.χ.card / state.p.val.toReal
-
-  | elem :: stream =>
-    modify λ state ↦ { state with χ := state.χ.erase elem }
-
-    let state ← get
-    let ⟨p, _, _⟩ := state.p
-
-    if ← PMF.bernoulli _ ‹p ≤ 1› then
-      modify λ state ↦ { state with χ := insert elem state.χ }
-
-    let state ← get
-    let ⟨p, _, _⟩ := state.p
-
-    if _h_card_eq_thresh : state.χ.card = thresh then
-      let _ ← state.χ.toList.traverse λ elem ↦ do
-        if ← PMF.bernoulli _ ‹p ≤ 1› then
-          modify λ state ↦ { state with χ := state.χ.erase elem }
-
-      let χ ← modifyGet λ state ↦
-        have := calc
-          p / 2 ≤ p := by exact ENNReal.half_le_self
-              _ ≤ 1 := ‹_›
-        (state.χ, { state with p := ⟨p / 2, by aesop, this⟩ })
-
-      if _h_card_eq_thresh : χ.card = thresh then throw default
-
-    estimateSize stream
-
-noncomputable def runEstimateSize
-  (stream : List <| Fin m)
-  (state : State m := initialState _)
-  : PMF <| Except Prob.Error <| Fin m :=
+  : ExceptT Unit PMF <| Fin m :=
   stream
-    |> estimateSize m ε δ
-    |>.run        -- Run ExceptT
-    |>.run' state -- Run StateT
+    |>.foldlM step (initialState m)
+    |>.map result
+  where
+    step (state : State m) (elem : Fin m) : ExceptT Unit PMF <| State m := do
+      let ⟨p, _, _⟩ := state.p
+      let χ := state.χ.erase elem
+
+      let χ :=
+        if ← PMF.bernoulli _ ‹p ≤ 1›
+        then insert elem χ
+        else χ
+
+      let state ←
+        if χ.card = thresh then
+          have : (1 : ℝ≥0∞) / 2 ≤ 1 := ENNReal.half_le_self
+
+          let χ ← χ.toList.filterM λ _ ↦ PMF.bernoulli _ this
+
+          have := calc
+            p / 2 ≤ p := by exact ENNReal.half_le_self
+                _ ≤ 1 := ‹_›
+
+          have : Multiset.Nodup ⟦χ⟧ := sorry
+
+          pure
+            { state with
+                p := ⟨p/2, by aesop⟩
+                χ := {val := ⟦χ⟧, nodup := this}
+            }
+        else
+          pure state
+
+      if state.χ.card = thresh then throw default
+
+      pure state
+
+    result (state : State m) : Fin m :=
+      Nat.floor <| state.χ.card / state.p.val.toReal
+
+-- open Classical in
+-- noncomputable def estimateSize
+--   (stream : List <| Fin m)
+--   : Prob.ProbM (State m) <| Fin m:= do
+--   match _h_stream_eq : stream with
+--   | [] =>
+--     let state ← get
+--     pure <| Nat.floor <| state.χ.card / state.p.val.toReal
+
+--   | elem :: stream =>
+--     modify λ state ↦ { state with χ := state.χ.erase elem }
+
+--     let state ← get
+--     let ⟨p, _, _⟩ := state.p
+
+--     if ← PMF.bernoulli _ ‹p ≤ 1› then
+--       modify λ state ↦ { state with χ := insert elem state.χ }
+
+--     let state ← get
+--     let ⟨p, _, _⟩ := state.p
+
+--     if _h_card_eq_thresh : state.χ.card = thresh then
+--       let _ ← state.χ.toList.traverse λ elem ↦ do
+--         if ← PMF.bernoulli _ ‹p ≤ 1› then
+--           modify λ state ↦ { state with χ := state.χ.erase elem }
+
+--       let χ ← modifyGet λ state ↦
+--         have := calc
+--           p / 2 ≤ p := by exact ENNReal.half_le_self
+--               _ ≤ 1 := ‹_›
+--         (state.χ, { state with p := ⟨p / 2, by aesop, this⟩ })
+
+--       if _h_card_eq_thresh : χ.card = thresh then throw default
+
+--     estimateSize stream
+
+-- noncomputable def runEstimateSize
+--   (stream : List <| Fin m)
+--   (state : State m := initialState _)
+--   : PMF <| Except Prob.Error <| Fin m :=
+--   stream
+--     |> estimateSize m ε δ
+--     |>.run        -- Run ExceptT
+--     |>.run' state -- Run StateT
 
 end CVM
--- Leanplayground/Prob/CVM/Results/Basic.lean
+++ Leanplayground/Prob/CVM/Results/Basic.lean
@@ -8,34 +8,34 @@ variable
   {m : ℕ+}
   {ε δ : Set.Ioo (α := ℝ) 0 1}
 
-lemma SatisfiesM_estimateSize_empty_list :
-  SatisfiesM (. = .ok 0) <| runEstimateSize m ε δ [] := by
-  simp only [runEstimateSize, estimateSize, initialState]
-  apply SatisfiesM.map_pre
-  apply SatisfiesM.bind_pre
-  apply SatisfiesM.map_pre
-  apply SatisfiesM.pure
-  apply SatisfiesM.pure
-  simp_all
+-- lemma SatisfiesM_estimateSize_empty_list :
+--   SatisfiesM (. = .ok 0) <| runEstimateSize m ε δ [] := by
+--   simp only [runEstimateSize, estimateSize, initialState]
+--   apply SatisfiesM.map_pre
+--   apply SatisfiesM.bind_pre
+--   apply SatisfiesM.map_pre
+--   apply SatisfiesM.pure
+--   apply SatisfiesM.pure
+--   simp_all
 
-example :
-  ∃ result : PMF {result // result = .ok 0},
-    Subtype.val <$> result = runEstimateSize m ε δ [] :=
-  have : SatisfiesM (. = .ok 0) <| runEstimateSize m ε δ [] :=
-    SatisfiesM_estimateSize_empty_list
-  by simp_all [SatisfiesM]
+-- example :
+--   ∃ result : PMF {result // result = .ok 0},
+--     Subtype.val <$> result = runEstimateSize m ε δ [] :=
+--   have : SatisfiesM (. = .ok 0) <| runEstimateSize m ε δ [] :=
+--     SatisfiesM_estimateSize_empty_list
+--   by simp_all [SatisfiesM]
 
-attribute [simp]
-  ExceptT.run StateT.run
-  get getThe MonadStateOf.get StateT.get
-  liftM monadLift MonadLift.monadLift ExceptT.lift
-  ExceptT.mk
-  Functor.map StateT.map ExceptT.map
-  bind StateT.bind ExceptT.bind ExceptT.bindCont
-  pure ExceptT.pure StateT.pure
+-- attribute [simp]
+--   ExceptT.run StateT.run
+--   get getThe MonadStateOf.get StateT.get
+--   liftM monadLift MonadLift.monadLift ExceptT.lift
+--   ExceptT.mk
+--   Functor.map StateT.map ExceptT.map
+--   bind StateT.bind ExceptT.bind ExceptT.bindCont
+--   pure ExceptT.pure StateT.pure
 
-example : runEstimateSize m ε δ [] = PMF.pure (Except.ok 0) := by
-  simp [estimateSize, runEstimateSize]
+-- example : runEstimateSize m ε δ [] = PMF.pure (Except.ok 0) := by
+--   simp [estimateSize, runEstimateSize]
 
 -- lemma SatisfiesM_estimateSize_error {stream : List <| Fin m} :
 --   SatisfiesM (. = .error default) <| runEstimateSize m ε δ stream := by
