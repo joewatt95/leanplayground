1f8714b384e604a4fbb783d45552fbf74a4a6f9d
Joe Watt <joewatt95@u.nus.edu>
2024-08-17 00:14:17 +0800

    strengthen loop invariant

--- Leanplayground/Prob/CVM/Algo.lean
+++ Leanplayground/Prob/CVM/Algo.lean
@@ -18,16 +18,37 @@ variable
   (ε δ : Set.Ioo (α := ℝ) 0 1)
   (xs : List <| Fin m)
 
-noncomputable abbrev thresh := Nat.ceil <| (12 / ε^2) * Real.log2 ((8*m) / δ)
+noncomputable abbrev thresh : ℕ+ :=
+  let thresh := ⌈(12 / ε^2) * Real.log2 ((8*m) / δ)⌉₊
+  have : 0 < thresh :=
+    have : (0 : ℝ) < (12 / ε^2) :=
+      have : (0 : ℝ) < 12 := Nat.ofNat_pos
+      have : (0 : ℝ) < ε^2 := by
+        have := ε.prop.1
+        simp_all only [Nat.ofNat_pos, gt_iff_lt, pow_pos]
+      div_pos ‹_› ‹_›
+    have : 0 < Real.log2 ((8*m) / δ) :=
+      have : (1 : ℝ) < 2 := by
+        simp_all only [gt_iff_lt, Nat.ofNat_pos, div_pos_iff_of_pos_left, Nat.one_lt_ofNat]
+      have : (1 : ℝ) < (8*m) / δ :=
+        have := calc
+          δ < (1 : ℝ) := by exact δ.prop.2
+          _ ≤ m := have := m.prop; by aesop
+          _ ≤ 8 * m := by aesop
+        have : (0 : ℝ) < δ := δ.prop.1
+        by duper [*, one_lt_div] {portfolioInstance := 1}
+      Real.logb_pos ‹_› ‹_›
+    by duper [*, Nat.ceil_pos, mul_pos] {portfolioInstance := 1}
+  ⟨thresh, this⟩
 
 @[ext]
 structure State where
   p : Set.Ioc (α := ℝ≥0∞) 0 1
-  χ : {S : Finset <| Fin m // S.card ≤ thresh m ε δ}
+  χ : {S : Finset <| Fin m // S.card < thresh m ε δ}
 
 noncomputable def initialState : State m ε δ where
   p := ⟨1, zero_lt_one, Preorder.le_refl _⟩
-  χ := ⟨∅, by simp_all only [Finset.card_empty, Nat.cast_zero, zero_le]⟩
+  χ := ⟨∅, have := thresh m ε δ |>.prop; by simp_all⟩
 
 -- instance : CommApplicative PMF where
 --   commutative_prod _ _ := by
@@ -49,36 +70,32 @@ noncomputable def estimateSize : ExceptT Unit PMF <| Fin m :=
       let ⟨p, (_ : 0 < p), (_ : p ≤ 1)⟩ := state.p
       let χ := state.χ
 
-      let χ :=
+      let χ' :=
         if ← PMF.bernoulli _ ‹p ≤ 1›
-        then χ
-        else
-          let ⟨χ, (_ : χ.card ≤ thresh _ _ _)⟩ := χ
-          let χ' := χ.erase x
-          have := calc
-            χ'.card ≤ χ.card       := by exact Finset.card_erase_le
-                  _ ≤ thresh _ _ _ := ‹_›
-          ⟨χ', by aesop⟩
+        then χ.val.erase x
+        else insert x <| χ.val.erase x
 
-      if _h_card_eq_thresh : χ.val.card < thresh m ε δ
+      let ⟨p, (_ : 0 < p), (_ : p ≤ 1)⟩ := state.p
+      if _h_card_eq_thresh : χ'.card < thresh m ε δ
       then return state else
 
-      let χ'attached : Finset {x : Fin m // x ∈ χ.val} ← χ
+      let χ''attached : Finset {x : Fin m // x ∈ χ.val} ← χ
         |>.val
         |>.attach
         |> Finset.filterM λ _ ↦
             have : ((1 : ℝ≥0∞) / 2) ≤ 1 := ENNReal.half_le_self
             PMF.bernoulli _ this
 
-      let χ' : Finset <| Fin m := Function.Embedding.subtype _ <$> χ'attached
+      let χ'' : Finset <| Fin m := Function.Embedding.subtype _ <$> χ''attached
 
-      have : ∀ x' ∈ χ', ∃ (pf : x' ∈ χ.val), ⟨x', pf⟩ ∈ χ'attached := by aesop
-      have : χ' ≤ χ.val := λ _ _ ↦ this _ ‹_› |>.1
+      have : ∀ x' ∈ χ'', ∃ (pf : x' ∈ χ.val), ⟨x', pf⟩ ∈ χ''attached := by aesop
+      have : χ'' ≤ χ.val := λ _ _ ↦ by duper [*] {portfolioInstance := 7}
+
       have := calc
-        χ'.card ≤ χ.val.card   := by exact Finset.card_le_card this
-              _ = thresh _ _ _ := by have := χ.prop; omega
+        χ''.card ≤ χ.val.card   := by exact Finset.card_le_card this
+               _ < thresh _ _ _ := χ.prop
 
-      let χ' : {S : Finset <| Fin m // S.card ≤ thresh m ε δ} := ⟨χ', this⟩
+      let χ'' : {S : Finset <| Fin m // S.card < thresh m ε δ} := ⟨χ'', this⟩
 
       let p' : Set.Ioc (α := ℝ≥0∞) 0 1 :=
         have := calc
@@ -86,9 +103,9 @@ noncomputable def estimateSize : ExceptT Unit PMF <| Fin m :=
               _ ≤ 1 := ‹_›
         ⟨p / 2, by aesop⟩
 
-      if _h_card_eq_thresh : χ.val.card = thresh m ε
+      if _h_card_eq_thresh : χ.val.card = thresh m ε δ
       then throw ()
-      else return { state with p := p', χ := χ' }
+      else return { state with p := p', χ := χ'' }
 
     result (state : State m ε δ) : Fin m :=
       Nat.floor <| state.χ.val.card / state.p.val.toReal
