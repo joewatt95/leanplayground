e96ed93f7137480d51e67ec743b251104629d401
joewatt95 <joewatt95@u.nus.edu>
2024-11-27 17:43:29 +0800

    t

--- Leanplayground/MathInLean/LinearAlgebra/VectorSpaces.lean
+++ Leanplayground/MathInLean/LinearAlgebra/VectorSpaces.lean
@@ -3,4 +3,14 @@ import Leanplayground.MathInLean.Utils.Tactic
 
 namespace VectorSpaces
 
+variable {K : Type*} [Field K] {V : Type*} [AddCommGroup V] [Module K V]
+
+def preimage {W : Type*} [AddCommGroup W] [Module K W] (φ : V →ₗ[K] W)
+  (H : Submodule K W)
+  : Submodule K V where
+  carrier := φ ⁻¹' H
+  zero_mem' := by simp_all
+  add_mem' := by aesop
+  smul_mem' := by aesop
+
 end VectorSpaces
