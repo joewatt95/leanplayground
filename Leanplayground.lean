f6670ffaf7f0633b7f74c98ad3e9e111a4e975db
Joe Watt <joewatt95@u.nus.edu>
2024-12-18 19:39:21 +0800

    fix compilation

--- Leanplayground.lean
+++ Leanplayground.lean
@@ -4,7 +4,7 @@ import Leanplayground.Choice.StandardChoice
 import Leanplayground.Logic
 import Leanplayground.MyNat
 import Leanplayground.MathInLean
-import Leanplayground.Prob
+-- import Leanplayground.Prob
 
 -- import Leanplayground.Natural4
 -- import Leanplayground.Primes.PrimeSieve
