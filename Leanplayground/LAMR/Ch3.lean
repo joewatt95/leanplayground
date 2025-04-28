5386120a397584d85ec4f39a19a377d7bc0275b3
Joe Watt <joewatt95@u.nus.edu>
2025-04-29 00:04:34 +0800

    wip

--- Leanplayground/LAMR/Ch3.lean
+++ Leanplayground/LAMR/Ch3.lean
@@ -4,96 +4,76 @@ import Mathlib.Data.List.Iterate
 import Leanplayground.MathInLean.Utils.Tactic
 
 def gcd (m n : ℕ) : ℕ :=
-  if h : n = 0
-  then m
+  if h : n = 0 then m
   else
-    have : n > 0 := by omega
-    have : m % n < n := Nat.mod_lt _ this
-    gcd n (m % n)
+    have : m % n < n := Nat.mod_lt _ <| by omega
+    gcd n <| m % n
 
 namespace List
 
-def rotations {α} (xs : List α) : List <| List α :=
-  iterate go xs xs.length
+abbrev rotations {α} (xs : List α) : List <| List α :=
+  iterate rotate xs xs.length
   where
-    go
+    @[reducible] rotate
       | [] => []
       | x :: xs => xs ++ [x]
 
-lemma test : ∀ {xs xs' : List α} {x},
-  x ∈ xs → xs' ∈ xs.rotations → x ∈ xs'
-  | [], _, _ => by simp [rotations]
-  | x :: xs, xs', x' => by
-    simp_all [rotations, rotations.go]
-    sorry
+@[simp]
+lemma rotate_length_eq_length {α n} {xs : List α} :
+  (rotations.rotate^[n] xs).length = xs.length :=
+  match n with
+  | 0 => by simp
+  | n + 1 => calc
+      (rotations.rotate^[n + 1] xs).length
 
-lemma nil_notin_rotations {α} :
-  ∀ {xs : List α}, [] ∉ xs.rotations
-  | [], _ | [_], _ => by simp [rotations] at *
-  | x :: x' :: xs, (_ : [] ∈ (x :: x' :: xs).rotations) =>
-    have ⟨m, (_ : m < xs.length), (_ : rotations.go^[m] (xs ++ [x, x']) = [])⟩ :
-      ∃ m < _, _ := by
-      simp_all [rotations, rotations.go]
+  _ = (rotations.rotate <| rotations.rotate^[n] xs).length := by
+    rw [Function.iterate_succ_apply']
 
-    match m with
-    | 0 => show ⊥ by simp_all
-    | m + 1 => by
-      simp_all [rotations, rotations.go]
-      sorry
+  _ = xs.length := go rotate_length_eq_length
+  where
+    go {xs ys : List α}
+      (_ : xs.length = ys.length) :
+      (rotations.rotate xs).length = ys.length :=
+      match xs, ys with
+      | [], [] | _ :: _, _ :: _ => by simp_all
 
-    -- have : [] ∈ (x' :: xs).rotations := by
-    --   simp [rotations, rotations.go] at *
-    --   sorry
+-- lemma length_eq_length_of_mem_rotations {α} {xs ys : List α}
+--   (_ : ys ∈ xs.rotations) : ys.length = xs.length := by
+--   aesop (add unsafe rotate_length_eq_length)
 
-    -- show ⊥ from nil_notin_rotations this
-
-def perms {α} : List α → List (List α)
+abbrev perms {α} : List α → List (List α)
   | [] => [[]]
-  | x :: xs =>
-    xs.perms.flatMap (rotations ∘ (x :: .))
+  | x :: xs => xs.perms.flatMap (rotations ∘ (x :: .))
 
-lemma length_eq_length_of_mem_rotations {α} :
-  ∀ {xs xs' : List α}, xs' ∈ rotations xs → xs'.length = xs.length
-  | [], _ => by simp [rotations]
+lemma length_eq_length_of_mem_perms {α} {xs ys : List α}
+  (_ : ys ∈ perms xs) : ys.length = xs.length :=
+  match xs with
+  | [] | _ :: _ => by aesop (add unsafe length_eq_length_of_mem_perms)
 
-  | x :: xs, x' :: xs' =>
-    sorry
-
-  | _ :: _, [] => by
-    sorry
-
-lemma length_rotations_eq {α} {xs : List α} :
-  (List.rotations xs).length = xs.length := by
-  simp [List.rotations, List.length_iterate]
-
--- lemma List.length_flatMap_rotations_eq {α} {xs : List <| List α} :
---   (xs.flatMap List.rotations).length = sorry :=
---   sorry
+lemma map_const_eq_replicate {α} {xs : List α} :
+  xs.map (λ _ ↦ c) = replicate (length xs) c :=
+  match xs with
+  | [] | _ :: _ => by
+    aesop (add unsafe map_const_eq_replicate) (add norm replicate)
 
 open Nat in
-lemma length_perms_eq_factorial_length {α} {xs xs' : List α}:
-  xs' ∈ perms xs → xs'.length = xs.length :=
+lemma length_perms_eq_factorial_length {α} {xs : List α}:
+  xs.perms.length = (xs.length)! :=
   match xs with
-  | [] => by simp [perms]
-  | y :: ys =>
-    have :
-      ∀ ys' ∈ perms ys, xs' ∈ rotations (y :: ys') →
-      xs'.length = ys.length + 1 := by
-      simp [rotations]
-      sorry
+  | [] => by simp
+  | x :: xs => calc
+      (List.perms <| x :: xs).length
 
-    show _ by simpa [perms]
+  _ = List.sum (xs |>.perms |>.map (List.length . + 1)) := by simp
 
-  -- (perms xs).length = (xs.length)! :=
-  -- sorry
+  _ = List.sum (xs |>.perms |>.map λ _ ↦ xs.length + 1) :=
+      congrArg _ <| by aesop (add unsafe length_eq_length_of_mem_perms)
 
-  -- | [] => by simp only [perms, List.length_cons, List.length_nil, zero_add, factorial_zero]
-  -- | x :: xs => calc
-  --     (List.perms <| x :: xs).length
-  -- _ = List.sum (xs |> perms |>.map (List.length . + 1)) := by
-  --     simp [perms, List.rotations]
-  -- _ = ((x :: xs).length)! := by
-  --     simp [factorial_succ]
-  --     sorry
+  _ = (xs.length + 1) * xs.perms.length := by
+      aesop (add norm map_const_eq_replicate) (add norm (by ring))
+
+  _ = (xs.length + 1) * (xs.length)! := by rw [length_perms_eq_factorial_length]
+
+  _ = ((x :: xs).length)! := by aesop
 
 end List
