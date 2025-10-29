2857db79e4e807060d5417d0091fc910b31ec5a6
Joe Watt <joewatt95@u.nus.edu>
2025-10-29 10:07:04 +0800

    bump

--- lakefile.lean
+++ lakefile.lean
@@ -7,28 +7,29 @@ package leanplayground where
 lean_lib Leanplayground where
 
 require "PatrickMassot" / verbose @
-  git "bd3b236511d9a8929791f20f775e98a9ef81697c"
+  git "c978de71e0241a2cf7e6010e562ead8d508115ef"
 
 require "ufmg-smite" / smt @
-  git "d6c048b99c80e93c42e7fb03ba5b14a74b00a836"
+  git "a62ff46dd0a9d2505c74f3160ec67c3e6d5e8adc"
 
 require "chasenorman" / Canonical @
-  git "v4.23.0"
+  git "v4.24.0"
 
 require "JOSHCLUNE" / Hammer @
-  git "v4.23.0"
+  git "v4.24.0"
 
-require "marcusrossel" / egg @
-  git "13abfc8437528eeb0088ef223e778c94b069dac7"
+require "Joewatt95" / egg from git
+  "https://github.com/Joewatt95/lean-egg" @
+  "64fedc4717140ce88635a128f4c9c21cd3ef3701"
 
 require "nomeata" / calcify @
   git "b89b823f26eb35a1d9ed57af2663128d6b3a35c2"
 
 require "nomeata" / loogle @
-  git "15beac7f2a5913571cba016e4b87fed907c54f7d"
+  git "ee8c0388917eb6b594f24e1865d5b83230229252"
 
 require "leanprover" / verso @
-  git "v4.23.0"
+  git "v4.24.0"
 
 require "leanprover-community" / mathlib @
-  git "v4.23.0"
+  git "v4.24.0"
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	PATH=$$PATH:~/.elan/toolchains/leanprover--lean4---v4.23.0/bin/ lake build
+	PATH=$$PATH:~/.elan/toolchains/leanprover--lean4---v4.24.0/bin/ lake build
