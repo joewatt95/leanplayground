d26451e787f3e147ca2d467e0e315171f50e6065
joewatt95 <joewatt95@u.nus.edu>
2025-11-30 19:18:57 +0800

    bump

--- lakefile.lean
+++ lakefile.lean
@@ -11,17 +11,17 @@ require "PatrickMassot" / verbose @
 
 require "Joewatt95" / smt from git
   "https://github.com/Joewatt95/lean-smt" @
-  "ccb6bde98c6931a7d4e02c1fb13f2afb19909c80"
+  "2fc72ca6feaefde3ca26ca3e3e69509d5c0a932d"
 
 require "chasenorman" / Canonical @
   git "v4.25.0"
 
 require "JOSHCLUNE" / Hammer @
-  git "v4.25.0"
+  git "v4.25.1"
 
 require "Joewatt95" / egg from git
   "https://github.com/Joewatt95/lean-egg" @
-  "9bb889b31d2029a5729ff06ce80248485c34e493"
+  "1f3caba4d7504adb747215ad981be979410762be"
 
 require "nomeata" / calcify @
   git "b89b823f26eb35a1d9ed57af2663128d6b3a35c2"
@@ -33,4 +33,4 @@ require "leanprover" / verso @
   git "v4.25.1"
 
 require "leanprover-community" / mathlib @
-  git "v4.25.1"
+  git "v4.25.2"
