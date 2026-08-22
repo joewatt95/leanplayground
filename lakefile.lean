f818433eebfef58c68a50cd20aaf4c43d3f22adf
joewatt95 <joewatt95@u.nus.edu>
2026-08-22 12:25:21 +0800

    t

--- lakefile.lean
+++ lakefile.lean
@@ -8,10 +8,9 @@ lean_lib Leanplayground where
 
 require "chasenorman" / Canonical @ git "65e13dfb3d308177f560ea3347dd7fc94749076e"
 require "JOSHCLUNE" / Hammer @ git "f6d189d1d7cfb34d28d447c1a67a118539ab44f4"
-require sos from git
-  "https://github.com/leanprover/sos" @ "38e718d21fa05c6b783a87622ed1d3a8d1d6cbe2"
+require sos from git "https://github.com/leanprover/sos"
+  @ "38e718d21fa05c6b783a87622ed1d3a8d1d6cbe2"
 
-require "nomeata" / calcify @ git "b89b823f26eb35a1d9ed57af2663128d6b3a35c2"
 require "nomeata" / loogle @ git "9f11169aaebf1ed1e7dcc4077f2aafe0fcf66fd0"
 
 require "PatrickMassot" / verbose @ git "a2f87b133d5b5e1b0b81d4fd7586b8750c08c5db"
