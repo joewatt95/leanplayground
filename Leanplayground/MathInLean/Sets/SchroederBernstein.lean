afe8ffbac6844b8dcee63707e0f94273e43754c7
joewatt95 <joewatt95@u.nus.edu>
2026-08-22 20:32:53 +0800

    t

--- Leanplayground/MathInLean/Sets/SchroederBernstein.lean
+++ Leanplayground/MathInLean/Sets/SchroederBernstein.lean
@@ -67,7 +67,7 @@ theorem schroeder_bernstein
 
     let h a := if a ∈ S₀ then g.invFun a else f a
 
-    have : LeftInverse g.invFun g := leftInverse_invFun ‹Injective g›
+    have : LeftInverse g.invFun g := leftInverse_invFun ‹_›
 
     have : Surjective h :=
       have : g.invFun '' S₀ ∪ f '' S₀ᶜ = univ := by grind [compl_union_self]
